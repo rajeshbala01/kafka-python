e72bdd35672b43c5159e4d157c1d282feb4f34e9
rajeshbala01 <rajesh.bala@outlook.com>
2020-05-11 21:18:02 +0530

    commit

--- SampleKafkaConsumer.py
+++ SampleKafkaConsumer.py
@@ -2,11 +2,11 @@ from kafka import KafkaConsumer, TopicPartition
 
 print('Start Consumer')
 consumer = KafkaConsumer(bootstrap_servers='ec2-3-80-198-180.compute-1.amazonaws.com:9092',
-                                 group_id='Consumer-group1' ,
+                                 group_id='Consumer-group2' ,
                                  auto_offset_reset='earliest',
                                  consumer_timeout_ms=1000)
 ##consumer.subscribe(['TestTopic1'])
-tp = TopicPartition('test-topic', 1)
+tp = TopicPartition('test-topic', 2)
 consumer.assign([tp])
 consumer.poll(1000)
 for message in consumer:
